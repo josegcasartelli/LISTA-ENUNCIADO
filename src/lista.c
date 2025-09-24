02085fce361996365e902b29876a59fa730e4023
Jose <jogonzalezc@fi.uba.ar>
2025-09-24 19:46:49 +0000

    cammbiando ifs lista_insertar

--- src/lista.c
+++ src/lista.c
@@ -66,11 +66,14 @@ bool lista_agregar(lista_t *lista, void *dato)
 
 bool lista_insertar(lista_t *lista, void *elemento, size_t posicion)
 {
-	if (!lista || posicion > lista->cantidad)
+	if (!lista)
 		return false;
 
 	if (posicion == lista->cantidad)
 		return lista_agregar(lista, elemento);
+
+	if (posicion > lista->cantidad)
+		return false;
 
 	nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
 	if (!nuevo_nodo)
